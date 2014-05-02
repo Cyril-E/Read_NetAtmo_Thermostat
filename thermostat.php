49e77fa8a3de8e6a986ae7986d4f1a853ad42dff
Cyril <cyril@ituilerie.com>
2014-05-02 17:41:16 +0200

    MAJ=>OK
    Mise a jour
    Fonctionnement OK

--- thermostat.php
+++ thermostat.php
@@ -3,7 +3,8 @@
 // Cyril E      http://www.ituilerie.com
 // Script inspiré de celui de Cédric Locqueneux. http://maison-et-domotique.com
 
-$password='xxxxxx';
+
+$password='xxxxxxx';
 $username='xxxxxxx';
 
 if (isset($_GET['box']))
@@ -11,8 +12,8 @@ if (isset($_GET['box']))
     $box=$_GET['box'];
 }
 
-$app_id = 'xxxxxxxxxx';
-$app_secret = 'xxxxxxxxxxxx';
+$app_id = 'xxxxxxx';
+$app_secret = 'xxxxxxx';
 
 $token_url = "https://api.netatmo.net/oauth2/token";
 $postdata = http_build_query(
@@ -54,38 +55,33 @@ $module2 = $json_devices["body"]["modules"][1]["_id"];
 $device3 = $json_devices["body"]["devices"][2]["_id"];
 $module3 = $json_devices["body"]["modules"][2]["_id"];
 
-echo "<br /> Data : " . $device1   . "<br />";
-echo "<br /> Data : " . $module1   . "<br />";
-echo "<br /> Data : " . $device2   . "<br />";
-echo "<br /> Data : " . $module2   . "<br />";
-echo "<br /> Data : " . $device3   . "<br />";
-echo "<br /> Data : " . $module3   . "<br />";
-
-
 $url_thermostat1="http://api.netatmo.net/api/getthermstate?access_token=" .  $params['access_token']."&device_id=".$device1."&module_id=".$module1;
 $value_thermostat1= file_get_contents($url_thermostat1);
-
-	$json_mesures_thermostat1 = json_decode($value_thermostat1, true);
-	$temperature_additionnel1 = $json_mesures_thermostat1[3]["measured"][2]["temperature"][0];
-	$co2_additionnel1 = $json_mesures_thermostat1["measured"][0]["temperature"][0][1];
-	$humidite_additionnel1 = $json_mesures_thermostat1["therm_relay_cmd"];
-
-echo "<br />";
-echo "<br />";
+$json_mesures_thermostat1 = json_decode($value_thermostat1, true);
+$temperature_thermostat1 = $json_mesures_thermostat1["body"]["measured"]["temperature"];
+$run_thermostat1 = $json_mesures_thermostat1["body"]["therm_relay_cmd"];
 
 
-var_dump(json_decode($value_thermostat1));
-echo "<br />";
-echo "<br />";
-var_dump(json_decode($value_thermostat1, true));
+$url_thermostat2="http://api.netatmo.net/api/getthermstate?access_token=" .  $params['access_token']."&device_id=".$device2."&module_id=".$module2;
+$value_thermostat2= file_get_contents($url_thermostat2);
+$json_mesures_thermostat2 = json_decode($value_thermostat2, true);
+$temperature_thermostat2 = $json_mesures_thermostat2["body"]["measured"]["temperature"];
+$run_thermostat2 = $json_mesures_thermostat2["body"]["therm_relay_cmd"];
 
-echo "<br />";
-echo "<br /> Data2 : " . $value_thermostat1   . "<br />";
-echo "<br /> Data2 : " . $json_mesures_thermostat1   . "<br />";
 
-echo "<br /> Data2 : " . $temperature_additionnel1   . "<br />";
-echo "<br /> Data2 : " . $co2_additionnel1   . "<br />";
-echo "<br /> Data2 : " . $humidite_additionnel1   . "<br />";
+$url_thermostat3="http://api.netatmo.net/api/getthermstate?access_token=" .  $params['access_token']."&device_id=".$device3."&module_id=".$module3;
+$value_thermostat3= file_get_contents($url_thermostat3);
+$json_mesures_thermostat3 = json_decode($value_thermostat3, true);
+$temperature_thermostat3 = $json_mesures_thermostat3["body"]["measured"]["temperature"];
+$run_thermostat3 = $json_mesures_thermostat3["body"]["therm_relay_cmd"];
+
+
+echo "<br /> Temperature Thermostat 1 : " . $temperature_thermostat1   . "<br />";
+echo "<br /> Etat Thermostat 1 : " . $run_thermostat1   . "<br />";    // Etat du thermostat, 0 chaudiere a l'arret, 100 chaudiere en marche
+echo "<br /> Temperature Thermostat 2 : " . $temperature_thermostat2   . "<br />";
+echo "<br /> Etat Thermostat 2 : " . $run_thermostat2   . "<br />";
+echo "<br /> Temperature Thermostat 3 : " . $temperature_thermostat3   . "<br />";
+echo "<br /> Etat Thermostat 3 : " . $run_thermostat3   . "<br />";
 
 
 ?>
